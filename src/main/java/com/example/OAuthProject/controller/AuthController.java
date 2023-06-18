f141585491a86bb89c395f1823f4cbb87c74aaee
Kat Nix <deutschkat29@gmail.com>
2023-06-18 15:43:54 -0500

    OAuth

--- src/main/java/com/example/OAuthProject/controller/AuthController.java
+++ src/main/java/com/example/OAuthProject/controller/AuthController.java
@@ -2,14 +2,14 @@ package com.example.OAuthProject.controller;
 
 
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-@org.springframework.stereotype.Controller
+@Controller
 @RequestMapping("/")
-public class Controller {
+public class AuthController {
 
     @GetMapping("/")
     public String getProtectedPage(Authentication authentication) {
